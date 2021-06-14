ff596b24da333629002cbd8430e87e4dece93120
net <alilousimo81@gmail.com>
2021-06-14 20:12:23 +0100

    virsion 1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '76e02e3935573e7eb7e9ff59b06de616'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '363ff01c-6334-4d21-a40e-b11e7100f771'
+};
